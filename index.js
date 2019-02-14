09222940f507023b1d3ee0faa992b1257a4e1bbb
Dani Lyn <dlyn1110@gmail.com>
2019-02-14 02:49:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0
+
+class Neighborhood() {
+  constructor(name) {
+    this.name = name
+    this.id = ++neighborhoodId
+    store.neighborhoods.push(this)
+  }
+}
